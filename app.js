9b4544ed2a498bf286965fcc3dd7bca0c36ec218
ram <ramanarayanan13@gmail.com>
2019-09-21 23:08:28 +0530

    routes creation

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.argv[2] || 3000;
+const auth = require('./routes/authRoutes');
 
 app.get('/', (_req, res) => res.send('working'));
 
 app.listen(port, () => console.log(`server running on ${port}`));
+app.use('/api/', auth);
