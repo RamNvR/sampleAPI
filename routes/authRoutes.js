faac00d60647e3c6f37202b4633715c3e6466b3a
ram <ramanarayanan13@gmail.com>
2019-09-22 23:39:15 +0530

    ff

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -47,18 +47,14 @@ router.post('/login', celebrate({
     User.find({ "username": req.body.username }, (err, doc) => {
         if (err) res.status(500).json({ status: 500 });
         console.log(doc[0]);
-        try {
-            bcrypt.compare(req.body.password, doc[0].password, (err, isOk) => {
-                if (err) res.status(500).json({ status: 500 });
-                console.log(isOk)
-                if (isOk) res.status(200).json({ status: 200 });
-                else res.status(403).json({
-                    status: 403
-                });
+        bcrypt.compare(req.body.password, doc[0].password, (err, isOk) => {
+            if (err) res.status(500).json({ status: 500 });
+            console.log(isOk)
+            if (isOk) res.status(200).json({ status: 200 });
+            else res.status(403).json({
+                status: 403
             });
-        } catch (error) {
-            console.log(error);
-        }
+        });
     });
 });
 
