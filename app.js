842d71a52b6e6f80bcee469bbf31381a4c27d3f6
ram <ramanarayanan13@gmail.com>
2019-09-22 20:42:28 +0530

    node engines

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.argv[2] || process.env.PORT || 8080;
+const port = process.argv[2] || 8080;
 const auth = require('./routes/authRoutes');
 const project = require('./routes/project');
 
